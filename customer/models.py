9887f7bae95c039cfcb2822f62999e748fc292c7
ahtesham-quraish <ahtesham.quraish@gmail.com>
2019-10-16 15:03:31 +0500

    fix

--- customer/models.py
+++ customer/models.py
@@ -3,14 +3,14 @@ from account.models import Category
 
 
 class Customer(models.Model):
-    fName = models.CharField(max_length=20)
-    lName = models.CharField(max_length=20)
+    fName = models.CharField(max_length=20, default='Pakistan', null=True, blank=True)
+    lName = models.CharField(max_length=20, default='Pakistan', null=True, blank=True)
     Address1 = models.CharField(max_length=300, default=None, null=True, blank=True)
     Address2 = models.CharField(max_length=300, default=None, null=True, blank=True)
     Phone  = models.IntegerField(default=None, null=True, blank=True)
     postal_code = models.CharField(max_length=20, default=None, null=True, blank=True)
     city = models.CharField(max_length=20)
-    company_name = models.CharField(max_length=20, default=None, null=True, blank=True)
+    company_name = models.CharField(max_length=200, default=None, null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE,  default='1')
     country  = models.CharField(max_length=20, default='Pakistan', null=True, blank=True)
     email = models.CharField(max_length=100)
